a18d1e7a4719b7903714144f8fdbb9b73a71ab82
Seth Fowler <mark.seth.fowler@gmail.com>
2014-04-23 15:29:35 -0700

    Use a cleaner approach to combine libraries. Fixes #1

--- Setup.hs
+++ Setup.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE RecordWildCards #-}
+
 import Control.Applicative
-import Control.Arrow
 import Control.Exception
-import Data.Maybe
+import Control.Monad
 import Distribution.PackageDescription
 import Distribution.Simple
 import Distribution.Simple.BuildPaths
@@ -9,8 +10,6 @@ import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
 import Distribution.Simple.Utils
 import Distribution.Simple.Program
-import Distribution.Simple.Program.Ar(createArLibArchive)
-import qualified Distribution.ModuleName as ModuleName
 import qualified Distribution.Verbosity as Verbosity
 import System.Directory
 import System.FilePath
@@ -40,31 +39,38 @@ customBuildHook pkg lbi usrHooks flags = do
 
     (libvoyeurDir, _, libDir) <- libvoyeurPaths
     let verbosity = fromFlag (buildVerbosity flags)
-        lib       = fromJust (library . localPkgDescr $ lbi)
-        bdir      = buildDir lbi
-        libObjs   = map (libObjPath libDir) [ "voyeur"
-                                            , "net"
-                                            , "env"
-                                            , "event"
-                                            , "util"
-                                            ]
-        pdb       = withPrograms lbi
-        makeProg  = fromMaybe (error "'make' not found!") (lookupKnownProgram "make" pdb)
+        runMake = runDbProgram verbosity makeProgram (withPrograms lbi)
 
-    (makeCmd, _) <- requireProgram verbosity makeProg pdb
     inDir libvoyeurDir $
-      runProgram verbosity makeCmd []
+      runMake []
 
     buildHook simpleUserHooks pkg lbi usrHooks flags
 
     notice verbosity "Relinking libvoyeur.."
 
-    hsObjs <- getHaskellObjects lib lbi bdir objExtension (splitObjs lbi)
-    let staticObjs = libObjs ++ hsObjs
-        libFile    = getLibraryName bdir lbi
+    let libObjs   = map (libObjPath libDir) [ "voyeur"
+                                            , "net"
+                                            , "env"
+                                            , "event"
+                                            , "util"
+                                            ]
 
-    removeFile libFile
-    createArLibArchive verbosity lbi libFile staticObjs
+        componentLibs = concatMap componentLibNames $ componentsConfigs lbi
+        addStaticObjectFile libName objName = runAr ["r", libName, objName]
+        runAr = runDbProgram verbosity arProgram (withPrograms lbi)
+
+    forM_ componentLibs $ \componentLib -> do
+      when (withVanillaLib lbi) $
+           let libName = buildDir lbi </> mkLibName componentLib
+           in mapM_ (addStaticObjectFile libName) libObjs
+
+      when (withProfLib lbi) $
+           let libName = buildDir lbi </> mkProfLibName componentLib
+           in mapM_ (addStaticObjectFile libName) libObjs
+
+      when (withSharedLib lbi) $
+           let libName = buildDir lbi </> mkSharedLibName buildCompilerId componentLib
+           in mapM_ (addStaticObjectFile libName) libObjs
 
 customCleanHook :: PackageDescription -> () -> UserHooks -> CleanFlags -> IO ()
 customCleanHook pkg v hooks flags = do
@@ -79,9 +85,6 @@ customCleanHook pkg v hooks flags = do
 
     cleanHook simpleUserHooks pkg v hooks flags
 
-makeProgram :: Program
-makeProgram = simpleProgram "make"
-
 libvoyeurPaths :: IO (FilePath, FilePath, FilePath)
 libvoyeurPaths = do
   curDir <- getCurrentDirectory
@@ -89,14 +92,15 @@ libvoyeurPaths = do
           curDir </> "libvoyeur" </> "include",
           curDir </> "libvoyeur" </> "build")
   
-libPath :: FilePath -> FilePath
-libPath name = name <.> dllExtension
+componentLibNames :: (ComponentName, ComponentLocalBuildInfo, [ComponentName]) -> [LibraryName]
+componentLibNames (_, LibComponentLocalBuildInfo {..}, _) = componentLibraries
+componentLibNames _                                       = []
+
+makeProgram :: Program
+makeProgram = simpleProgram "make"
 
 libObjPath :: FilePath -> FilePath -> FilePath
 libObjPath dir name = dir </> name <.> objExtension
-
-asStaticLib :: FilePath -> FilePath
-asStaticLib lname = mkLibName (LibraryName lname)
 
 inDir :: FilePath -> IO a -> IO a
 inDir dir act = do
@@ -104,34 +108,6 @@ inDir dir act = do
   bracket_ (setCurrentDirectory dir)
            (setCurrentDirectory curDir)
            act
-
-getHaskellObjects :: Library -> LocalBuildInfo -> FilePath -> String -> Bool -> IO [FilePath]
-getHaskellObjects lib lbi pref wanted_obj_ext allow_split_objs
-  | splitObjs lbi && allow_split_objs = do
-        let splitSuffix = if compilerVersion (compiler lbi) <
-                             Version [6, 11] []
-                          then "_split"
-                          else "_" ++ wanted_obj_ext ++ "_split"
-            dirs = [ pref </> (ModuleName.toFilePath x ++ splitSuffix)
-                   | x <- libModules lib ]
-        objss <- mapM getDirectoryContents dirs
-        let objs = [ dir </> obj
-                   | (objs',dir) <- zip objss dirs, obj <- objs',
-                     let obj_ext = takeExtension obj,
-                     '.':wanted_obj_ext == obj_ext ]
-        return objs
-  | otherwise  =
-        return [ pref </> ModuleName.toFilePath x <.> wanted_obj_ext
-               | x <- libModules lib ]
-
-getLibraryName :: FilePath -> LocalBuildInfo -> FilePath
-getLibraryName pref lbi = getLibName $ filter isLib $ componentsConfigs lbi
-    where isLib (CLibName, _, _) = True
-          isLib _ = False
-          getLibName [(_,clbi,_)]
-              | LibraryName lname <- head (componentLibraries clbi) = pref </> asStaticLib lname
-          getLibName [] = error "getLibName: No libraries found!"
-          getLibName _ = error "getLibName: more than one library found!"
 
 type Lifter a b = (a -> a) -> b -> b
 
@@ -149,9 +125,3 @@ onLocalLibBuildInfo = onLocalPkgDescr . onLibrary . onLibBuildInfo
 
 onIncludeDirs :: Lifter [FilePath] BuildInfo
 onIncludeDirs f libbi = libbi { includeDirs = f (includeDirs libbi) }
-
-onExtraLibs :: Lifter [FilePath] BuildInfo
-onExtraLibs f libbi = libbi { extraLibs = f (extraLibs libbi) }
-
-onExtraLibDirs :: Lifter [FilePath] BuildInfo
-onExtraLibDirs f libbi = libbi { extraLibDirs = f (extraLibDirs libbi) }
